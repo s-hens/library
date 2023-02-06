157d49b0b4ffeb7a94dc7635f9dcfe44418f90dd
Stephanie Henson <108551327+s-hens@users.noreply.github.com>
2023-02-06 17:26:15 +0000

    Refine CSS, link function addBook to 'new book' form elements

--- javascript.js
+++ javascript.js
@@ -2,29 +2,49 @@ let myLibrary = [
     {
         title: "Norse Mythology",
         author: "Neil Gaiman",
-        read: "Yes",
-        rating: "Great"
+        readStatus: "Read",
+        rating: "5",
+        notes: "One of my favourites from Gaiman"
     },
     {
         title: "Gone Girl",
         author: "Gillian Flynn",
-        read: "Yes",
-        rating: "Good"
+        readStatus: "Read",
+        rating: "4",
+        notes: "Wish I hadn't seen the movie first so I could have been really surprised by the twist"
     }
 ];
 
 let bookList = document.getElementById("booklist");
 
-function Book(title, author, read, rating) {
+function Book(title, author, readStatus, rating, notes) {
     this.title = title;
     this.author = author;
-    this.read = read;
+    this.readStatus = readStatus;
     this.rating = rating;
+    this.notes = notes;
 }
 
 function display(book) {
     let div = document.createElement("div");
-    div.innerHTML = `${book.title} by ${book.author}<br>Read? ${book.read}<br>Rated: ${book.rating}`;
+    let star = `<span class="material-icons-sharp">star_rate</span>`;
+    let emptyStar = `<span class="material-icons-sharp">star_outline</span>`;
+    div.innerHTML = `
+    <div class="cardinfo">
+    <span class="title">${book.title}</span>
+    by <span class="author">${book.author}</span>
+    </div>
+    <div class="cardinfo">
+    <h3>Status:</h3> ${book.readStatus}
+    </div>
+    <div class="cardinfo">
+    <h3>Rating:</h3> ${star.repeat(book.rating)}${emptyStar.repeat(5 - book.rating)} 
+    </div>
+    <div class="cardinfo">
+    <h3>Notes:</h3> ${book.notes}
+    </div>
+    <div class="buttons"><button class="edit"><span class="material-icons-sharp">edit</span> Edit</button><button class="delete"><span class="material-icons-sharp">delete</span> Delete</button></div>
+    `;
     div.classList.add("card");
     bookList.appendChild(div);
 }
@@ -32,14 +52,40 @@ function display(book) {
 myLibrary.forEach(book => display(book));
 
 function addBook() {
-    let title = prompt(`Title:`);
-    let author = prompt(`Author:`);
-    let read = prompt(`Have you read this book?`);
-    let rating = prompt(`If you have read it, how do you rate it?`);
-    let myBook = new Book(title, author, read, rating);
+    //Prevent page refresh on form submission
+    event.preventDefault();
+    //Get title
+    let title = document.getElementById("title").value;
+    //Get author
+    let author = document.getElementById("author").value;
+    //Get status
+    let readStatus;
+    for (i = 0; i < 3; i++) {
+        if (document.getElementsByName("read")[i].checked) {
+            readStatus = document.getElementsByName("read")[i].value;
+        }
+    }
+    //Get rating
+    let rating;
+    for (i = 0; i < 5; i++) {
+        if (document.getElementsByName("rating")[i].checked) {
+            rating = document.getElementsByName("rating")[i].value;
+        }
+    }
+    //Get notes
+    let notes = document.getElementById("notes").value;
+    if (!notes) {
+        notes = "n/a";
+    };
+    //Make a new book object and add it to library array
+    let myBook = new Book(title, author, readStatus, rating, notes);
     myLibrary.push(myBook);
+    //Display new book on page
     display(myBook);
-    //console.table(myLibrary);
 }
 
-addBook();
+let submit = document.getElementById("submit");
+
+submit.addEventListener("click", addBook);
+
+//addBook();
