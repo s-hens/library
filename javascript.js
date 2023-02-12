76376c854e914de50807cf72ce9a9bce9b948473
Stephanie Henson <108551327+s-hens@users.noreply.github.com>
2023-02-12 00:49:26 +0000

    Lint

--- javascript.js
+++ javascript.js
@@ -15,7 +15,8 @@ const myLibrary = [
     title: 'The Fellowship of the Ring',
     author: 'J.R.R. Tolkien',
     readStatus: 'Currently reading',
-    notes: 'Several hours in. Maybe soon the boys will actually get out of the Shire.',
+    notes:
+      'Several hours in. Maybe soon the boys will actually get out of the Shire.',
   },
   {
     title: 'Stardust',
@@ -29,17 +30,18 @@ const myLibrary = [
     author: 'Kikuko Tsumura',
     readStatus: 'Read',
     rating: '4',
-    notes: 'This was really enjoyable. Eerie and otherworldly, yet funny and heartwarming at the same time.',
+    notes:
+      'This was really enjoyable. Eerie and otherworldly, yet funny and heartwarming at the same time.',
   },
 ];
 
-const bookList = document.querySelector("main");
+const bookList = document.querySelector('main');
 
-const formDialog = document.getElementById("new-edit-dialog");
+const formDialog = document.getElementById('new-edit-dialog');
 const openForm = document.getElementById('open-form');
 const closeForm = document.getElementById('close-form');
 
-const form = document.getElementById("new-edit-book");
+const form = document.getElementById('new-edit-book');
 
 const readStatusButtons = document.getElementsByName('read');
 const readButton = document.getElementById('read');
@@ -55,9 +57,11 @@ bookList.addEventListener('click', editBook);
 openForm.addEventListener('click', showForm);
 closeForm.addEventListener('click', showForm);
 
-form.addEventListener("submit", addBook)
+form.addEventListener('submit', addBook);
 
-readStatusButtons.forEach((button) => button.addEventListener('change', enableRating));
+readStatusButtons.forEach((button) =>
+  button.addEventListener('change', enableRating)
+);
 
 stars.forEach((star) => star.addEventListener('click', addStars));
 stars.forEach((star) => star.addEventListener('mouseover', addStars));
@@ -111,7 +115,7 @@ function editBook(e) {
     document.querySelector('#edit-head').style.display = 'block';
     // Find the book in the array
     let bookIndex;
-    if (e.target.classList.contains("material-icons-sharp")) {
+    if (e.target.classList.contains('material-icons-sharp')) {
       bookIndex = e.target.parentNode.parentNode.parentNode.getAttribute('num');
       console.log(bookIndex);
     } else {
@@ -123,21 +127,23 @@ function editBook(e) {
     // Fill form with book details
     document.getElementById('title').value = bookData.title;
     document.getElementById('author').value = bookData.author;
-    document.querySelector(`input[value="${bookData.readStatus}"`).checked = true;
+    document.querySelector(
+      `input[value="${bookData.readStatus}"`
+    ).checked = true;
     document.getElementById('notes').value = bookData.notes;
     if (bookData.rating) {
       enableRating();
       document.querySelector(`input[value="${bookData.rating}"`).checked = true;
       checkStars();
     } else {
-      rating.style.visibility = "hidden";
+      rating.style.visibility = 'hidden';
       checkStars();
     }
-}
+  }
 }
 
 function display(book) {
-  const bookCard = document.createElement("article");
+  const bookCard = document.createElement('article');
   if (!book.rating) {
     bookCard.innerHTML = `
       <section><h2>${book.title}</h2>by <h4>${book.author}</h4></section>
@@ -152,13 +158,15 @@ function display(book) {
     bookCard.innerHTML = `
       <section><h2>${book.title}</h2>by <h4>${book.author}</h4></section>
       <section><h3>Status:</h3> ${book.readStatus}</section>
-      <section><h3>Rating:</h3> ${star.repeat(book.rating)}${emptyStar.repeat(5 - book.rating)}</section>
+      <section><h3>Rating:</h3> ${star.repeat(book.rating)}${emptyStar.repeat(
+      5 - book.rating
+    )}</section>
       <section><h3>Notes:</h3> ${book.notes}</section>
       <div class="buttons"><button class="edit"><span class="material-icons-sharp edit">edit</span> Edit</button><button class="delete"><span class="material-icons-sharp delete">delete</span> Delete</button></div>
         `;
   }
   bookList.appendChild(bookCard);
-  bookCard.setAttribute("num", document.querySelectorAll("article").length - 1);
+  bookCard.setAttribute('num', document.querySelectorAll('article').length - 1);
 }
 
 myLibrary.forEach((book) => display(book));
@@ -178,12 +186,12 @@ function showForm() {
 
 function enableRating() {
   if (readButton.checked) {
-    rating.style.visibility = "visible";
-    document.getElementById("1").setAttribute("required", "");
+    rating.style.visibility = 'visible';
+    document.getElementById('1').setAttribute('required', '');
   } else {
-    rating.style.visibility = "hidden";
+    rating.style.visibility = 'hidden';
     stars.forEach((star) => (star.style.color = '#BAB0A1'));
-    document.getElementById("1").removeAttribute("required", "");
+    document.getElementById('1').removeAttribute('required', '');
     for (i = 0; i < 5; i++) {
       document.getElementsByName('rating')[i].checked = false;
     }
@@ -207,7 +215,10 @@ function checkStars() {
     stars.forEach((star) => {
       // When user removes mouse from stars, stars revert to displaying the rating which was last selected with a click
       // So an accidental mouse in/out can't appear to change the rating
-      if (Number(star.getAttribute('for')) <= Number(rating.querySelector('input:checked').getAttribute('id'))) {
+      if (
+        Number(star.getAttribute('for')) <=
+        Number(rating.querySelector('input:checked').getAttribute('id'))
+      ) {
         star.style.color = '#4DB6AC';
       } else {
         star.style.color = '#BAB0A1';
